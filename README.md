44cf789dcdba3837edd3f8cfe160490d1b915253
atsumih <atsumih@casio.co.jp>
2016-03-29 15:43:20 +0900

    1st

---
+++ README.md
@@ -0,0 +1,35 @@
+img2excel
+====
+make a Excel mosaic picture from image.
+
+## Description
+img2excel.py makes make a Excel mosaic picture from image.
+
+！[Original](namiura_small.jpg "Original Image")
+->
+！[Excel File](namiura_excel_small.jpg "Excel File")
+
+## Synopsis
+
+ python img2excel.py  [-c columns ] [-e excel_file] image_file
+
+
+## Example
+```
+python img2excel.py namiura.jpg
+python img2excel.py -c 20 -e test.xlsx namiura.jpg
+```
+
+## Requirement
+[openpyxl](https://openpyxl.readthedocs.org/)
+
+<!--
+## See also
+-->
+
+## Licence
+Copyright (c) 2016 zom-1, Released under the
+ [MIT](http://opensource.org/licenses/mit-license.php) license
+
+## Author
+[zom-1](https://github.com/zom-1)
---
+++ img2excel.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# coding: utf-8
+__author__ = "zom-1"
+__version__ = "0.5.1"
+__date__ = "2016/03/18"
+
+import argparse
+import os
+from PIL import Image # Pillow or PIL
+from openpyxl import Workbook # library to r/w excel -> https://openpyxl.readthedocs.org/
+from openpyxl.styles import PatternFill 
+
+def img2Excel(imgFile, excelFile, columns):
+    ''' make a Excel mosaic picture from image '''
+    try:
+        img=Image.open(imgFile)
+    except IOError:
+        print 'cannot open', imgFile
+        quit()
+
+    # make small image
+    orgWidth,orgHight = img.size
+    smlWidth=columns
+    smlHeight=smlWidth*orgHight/orgWidth
+    smlImg  = img.resize((smlWidth,smlHeight),Image.ANTIALIAS)
+
+    # make excel file
+    wb = Workbook() # new excel, TODO: error check
+    ws1 = wb.active
+    ws1.title = excelFile
+    for y in range(0,smlHeight):
+      for x in range(0,smlWidth):
+        r,g,b=smlImg.getpixel((x,y))
+        c="{0:0>2X}{1:0>2X}{2:0>2X}".format(r,g,b) # hex, ex)FF88CC
+        ws1.cell(row = y+1, column = x+1, value=" ").fill = PatternFill(fill_type='solid',start_color=c, end_color=c)
+
+    # n2A1 : column num to A1 style : 1->'A',2->'B'... 27->'AA',28->'AB'
+    n2A1 = lambda n: (chr(((n-1)//26)+64) if n>26 else '') + chr((n-1)%26+65)
+    for n in range(1,smlWidth+1):
+        ws1.column_dimensions[n2A1(n)].width=2.5 # set column width, TODO: better way ???
+
+    wb.save(excelFile)
+
+img2Excel('namiura.jpg', 'namiura.elsx', 80)
+
+if __name__ == '__main__':
+    # args parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--columns", default=50, type=int, help="columns of Excel")
+    parser.add_argument("-e", "--excel_file",  default="",  help="Excel file")
+    parser.add_argument("image_file", help="output a Excel of a given image")
+    args=parser.parse_args()
+    if (args.excel_file): # if empty, use image basename+'xlsx'
+      excelFile=args.excel_file
+    else:
+      excelFile=os.path.splitext(os.path.basename(imgFile))[0]+".xlsx" #
+
+    # exec
+    img2Excel(args.image_file, args.excel_file, args.columns)
